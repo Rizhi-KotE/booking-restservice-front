363e9a64e80f0fe4183f9221a93adfbff278982a
Rizhi_KotE <d1mqn1ch@mail.ru>
2017-04-25 18:24:06 +0300

    send meeting

--- src/MeetingCreateForm.js
+++ src/MeetingCreateForm.js
@@ -2,16 +2,82 @@ import React from "react";
 import {ControlLabel, FormControl, FormGroup} from "react-bootstrap";
 import update from "react-addons-update";
 import api from "./MeetingRestClient";
+import PropTypes from "prop-types";
+import "react-datetime/css/react-datetime.css";
+import moment from 'moment-js'
 
-export default class MeetingCreateForm extends React.Component {
+import DateTime from "react-datetime";
+
+
+class MeetingCreateForm extends React.Component {
+
+    render = () => {
+        let userSelects = this.props.users.map((user, index) => {
+            return <option key={user.id} value={index}>{user.username}</option>
+        });
+        let roomSelects = this.props.rooms.map((room, index) => {
+            return <option key={room.id} value={index}>
+                {`${room.id}: ${room.officeHoursBegin} - ${room.officeHoursEnd}`}
+            </option>
+        });
+        return <form onSubmit={event => {
+            event.preventDefault();
+            this.props.handleSubmit()
+        }}>
+            <FormGroup>
+                <ControlLabel>Employer</ControlLabel>
+                <FormControl componentClass="select" disabled={this.props.users.length === 0}
+                             onChange={event => this.props.selectUser(event.target.value)}>
+                    {userSelects}
+                </FormControl>
+            </FormGroup>
+            <FormGroup>
+                <ControlLabel>Room №</ControlLabel>
+                <FormControl componentClass="select" disabled={this.props.rooms.length === 0}
+                             onChange={event => this.props.selectRoom(event.target.value)}>
+                    {roomSelects}
+                </FormControl>
+            </FormGroup>
+            <FormGroup>
+                <ControlLabel>Begin</ControlLabel>
+                <DateTime
+                    type="datetime"
+                    dateFormat="YYYY-MM-DD"
+                    timeFormat="HH:mm"
+                    value={this.props.startTime}
+                    onChange={moment => this.props.changeTime(moment)}/>
+            </FormGroup>
+            <FormGroup>
+                <ControlLabel>Duration (Minutes)</ControlLabel>
+                <FormControl
+                    value={this.props.startTime}
+                    onChange={event => this.props.changeDuration(event.target.value)}/>
+            </FormGroup>
+            <FormControl type="submit" className="btn btn-default"/>
+        </form>
+    }
+}
+
+MeetingCreateForm.propTypes = {
+    users: PropTypes.array,
+    rooms: PropTypes.array,
+    selectUser: PropTypes.func.isRequired,
+    selectRoom: PropTypes.func.isRequired,
+    changeTime: PropTypes.func.isRequired,
+    changeDuration: PropTypes.func.isRequired,
+    handleSubmit: PropTypes.func.isRequired,
+};
+
+MeetingCreateForm.defaultProps = {
+    users: [],
+    rooms: []
+};
+
+export default class MeetingCreateComponent extends React.Component {
     constructor() {
         super();
         this.state = {
-            meeting: {
-                startTime: '1111111'
-            },
-            users: [],
-            rooms: []
+            meeting: {}
         };
     };
 
@@ -32,9 +98,15 @@ export default class MeetingCreateForm extends React.Component {
             })))
     };
 
-    handleSubmit = event => {
-        event.preventDefault()
-        api.meeting.post(this.state.meeting)
+    handleSubmit = () => {
+        api.meeting.post({
+            user: this.state.meeting.user,
+            room: this.state.meeting.room,
+            meetingDateBegin: this.state.meeting.meetingDateBegin.format('YYYY-MM-DD HH:mm'),
+            meetingDateEnd: this.state.meeting.meetingDateBegin.clone()
+                .add(this.state.meeting.duration, 'm').format('YYYY-MM-DD HH:mm'),
+            submitDate: moment().format('YYYY-MM-DD HH:mm:ss')
+        })
             .then(console.log, console.log)
     };
 
@@ -54,104 +126,36 @@ export default class MeetingCreateForm extends React.Component {
         }))
     };
 
-    changeMeetingDate = event => {
+    changeMeetingDate = date => {
+        this.setState(update(this.state, {
+            meeting: {
+                meetingDateBegin: {$set: date.clone()},
+                meetingDateEnd: {$set: date.clone().add(this.state.meeting.duration, 'm')}
+            }
+        }))
+    };
 
-    }
+    changeDuration = duration => {
+        this.setState(update(this.state, {
+            meeting: {
+                duration: {$set: duration},
+                meetingDateEnd: {
+                    $set: this.state.meeting.meetingDateBegin
+                        .clone()
+                        .add(duration, 'm')
+                }
+            }
+        }));
+    };
 
-    render = () => {
-        let userSelects = this.state.users.map((user, index) => {
-            return <option key={user.id} value={index}>{user.username}</option>
-        });
-        let roomSelects = this.state.rooms.map((room, index) => {
-            return <option key={room.id}
-                           value={index}>{`${room.id}: ${room.officeHoursBegin} - ${room.officeHoursEnd}`}</option>
-        });
-        return <form
-            onSubmit={this.handleSubmit}>
-            <FormGroup>
-                <ControlLabel>Employer</ControlLabel>
-                <FormControl componentClass="select"
-                             onChange={event => this.selectUser(event.target.value)}>
-                    {userSelects}
-                </FormControl>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Room №</ControlLabel>
-                <FormControl componentClass="select"
-                             onChange={event => this.selectRoom(event.target.value)}>
-                    {roomSelects}
-                </FormControl>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Begin</ControlLabel>
-                <FormControl
-                    type="date"
-                    value={this.state.startTime}
-                    onChange={event => this.setState(update(this.state, {
-                        startTime: {$set: event.target.value}
-                    }))}/>
-            </FormGroup>
-            <FormGroup>
-                <ControlLabel>Duration (Minutes)</ControlLabel>
-                <FormControl
-                    value={this.state.startTime}
-                    onChange={event => this.setState(update(this.state, {
-                        duration: {$set: event.target.value}
-                    }))}/>
-            </FormGroup>
-            <button className="btn btn-default" type="submit">Submit</button>
-        </form>
-    }
-}
+    render = () =>
+        <MeetingCreateForm
+            users={this.state.users}
+            rooms={this.state.rooms}
+            selectUser={this.selectUser.bind(this)}
+            selectRoom={this.selectRoom.bind(this)}
+            changeTime={this.changeMeetingDate.bind(this)}
+            changeDuration={this.changeDuration.bind(this)}
+            handleSubmit={this.handleSubmit.bind(this)}/>
 
-var a = {
-    "timestamp": "2017-04-25T12:55:28.624+0000",
-    "status": 400,
-    "error": "Bad Request",
-    "exception": "org.springframework.web.bind.MethodArgumentNotValidException",
-    "errors": [{
-        "codes": ["NotNull.meeting.meetingDateBegin", "NotNull.meetingDateBegin", "NotNull.java.time.LocalDateTime", "NotNull"],
-        "arguments": [{
-            "codes": ["meeting.meetingDateBegin", "meetingDateBegin"],
-            "arguments": null,
-            "defaultMessage": "meetingDateBegin",
-            "code": "meetingDateBegin"
-        }],
-        "defaultMessage": "должно быть задано",
-        "objectName": "meeting",
-        "field": "meetingDateBegin",
-        "rejectedValue": null,
-        "bindingFailure": false,
-        "code": "NotNull"
-    }, {
-        "codes": ["NotNull.meeting.submitDate", "NotNull.submitDate", "NotNull.java.time.LocalDateTime", "NotNull"],
-        "arguments": [{
-            "codes": ["meeting.submitDate", "submitDate"],
-            "arguments": null,
-            "defaultMessage": "submitDate",
-            "code": "submitDate"
-        }],
-        "defaultMessage": "должно быть задано",
-        "objectName": "meeting",
-        "field": "submitDate",
-        "rejectedValue": null,
-        "bindingFailure": false,
-        "code": "NotNull"
-    }, {
-        "codes": ["NotNull.meeting.meetingDateEnd", "NotNull.meetingDateEnd", "NotNull.java.time.LocalDateTime", "NotNull"],
-        "arguments": [{
-            "codes": ["meeting.meetingDateEnd", "meetingDateEnd"],
-            "arguments": null,
-            "defaultMessage": "meetingDateEnd",
-            "code": "meetingDateEnd"
-        }],
-        "defaultMessage": "должно быть задано",
-        "objectName": "meeting",
-        "field": "meetingDateEnd",
-        "rejectedValue": null,
-        "bindingFailure": false,
-        "code": "NotNull"
-    }],
-    "message": "Validation failed for object='meeting'. Error count: 3",
-    "path": "/meeting"
 }
